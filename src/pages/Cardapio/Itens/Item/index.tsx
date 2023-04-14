2ccf6ec9092044a9222080706e205ab6b9e928ed
Rodrigo Carneiro <carneironline@gmail.com>
2023-04-14 01:23:35 -0300

    Imports dinâmicos

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,6 +1,6 @@
 import styles from './Item.module.scss'
-import logo from 'assets/logo.svg'
 import cardapio from '../itens.json'
+import classNames from 'classnames'
 
 type Props = typeof cardapio[0]
 
@@ -12,11 +12,17 @@ export default function Item(props: Props) {
         size,
         serving,
         price,
+        photo
     } = props
+
+    const classCategories = classNames({
+        [styles.item__tipo]: true,
+        [styles[`item__tipo__${category.label.toLocaleLowerCase()}`]]: true
+    })
 
     return (
         <div className={styles.item}>
-            <div className={styles.item__imagem}><img src={logo} alt={title} /></div>
+            <div className={styles.item__imagem}><img src={photo} alt={title} /></div>
             <div className={styles.item__descricao}>
                 <div className={styles.item__titulo}>
                     <h2>{title}</h2>
@@ -24,7 +30,7 @@ export default function Item(props: Props) {
                 </div>
 
                 <div className={styles.item__tags}>
-                    <div className={styles.item__tipo}> {category.label}</div>
+                    <div className={classCategories}> {category.label}</div>
                     <div className={styles.item__porcao}>{size}g</div>
                     <div className={styles.item__qtdpessoas}>serve {serving} pessoa{serving === 1 ? '' : 's'}</div>
                     <div className={styles.item__valor}>{price.toFixed(2)}</div>
