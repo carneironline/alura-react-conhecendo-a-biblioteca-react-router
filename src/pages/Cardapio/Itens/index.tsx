87617a41f41b3154d077308c0d3905390386a326
Rodrigo Carneiro <carneironline@gmail.com>
2023-04-14 01:43:36 -0300

    Refatoração do ordenar

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -27,16 +27,22 @@ export default function Itens(props: Props) {
     function ordenar(novaLista: typeof cardapio) {
         switch (ordenador) {
             case 'porcao':
-                return novaLista.sort((a, b) => a.size > b.size ? 1 : -1)
+              return ordenarPropriedadeCrescente(novaLista, 'size');
             case 'qtd_pessoas':
-                return novaLista.sort((a, b) => a.serving > b.serving ? 1 : -1)
+              return ordenarPropriedadeCrescente(novaLista, 'serving');
             case 'preco':
-                return novaLista.sort((a, b) => a.price > b.price ? 1 : -1)
-        
+              return ordenarPropriedadeCrescente(novaLista, 'price');
             default:
-                return novaLista;
-        }
+              return novaLista;
+          }
     }
+
+    function ordenarPropriedadeCrescente (
+        lista: typeof cardapio,
+        propriedade: 'size' | 'serving' | 'price'
+      ) {
+        return lista.sort((a, b) => (a[propriedade] > b[propriedade] ? 1 : -1));
+      };
 
     useEffect(() => {
         const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id))
