be23d717d989f3a4240fd6e57d7a461525cf02ec
Rodrigo Carneiro <carneironline@gmail.com>
2023-04-14 22:26:13 -0300

    Terminando a tela Início

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
+import Sobre from 'pages/Sobre';
 import Menu from 'components/Menu';
 import Template from 'components/Template';
 
@@ -14,6 +15,7 @@ export default function AppRouter() {
                     <Route path='/' element={<Template />} >
                         <Route index element={<Inicio />} />
                         <Route path='cardapio' element={<Cardapio />} />
+                        <Route path='sobre' element={<Sobre />} />
                     </Route>
                 </Routes>
             </main>
